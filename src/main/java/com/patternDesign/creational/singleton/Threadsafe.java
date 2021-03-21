c99095bd7e82a428b13f241bb0f1a590bc70ab14
Rafael Chavez <rafaelchavez@MacBook-Pro-de-Rafael.local>
2021-03-21 17:33:37 -0600

    Comment more clear

--- src/main/java/com/patternDesign/creational/singleton/Threadsafe.java
+++ src/main/java/com/patternDesign/creational/singleton/Threadsafe.java
@@ -8,7 +8,7 @@ public class Threadsafe {
     public static Threadsafe getInstance(){
         if (instance == null){
             synchronized (Threadsafe.class) {
-                if (instance == null) // If repeated due the first one is not thread safe
+                if (instance == null) // If-null repeated due the first one is not thread safe
                     instance = new Threadsafe();
             }
         }
